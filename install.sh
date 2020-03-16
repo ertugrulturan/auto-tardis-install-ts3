81b81d63984973be44f073d44a1fd33355f509ea
OBir.Ninja <60829297+obirninja@users.noreply.github.com>
2020-03-16 15:19:31 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ apt-get update && apt-get upgrade -y
 yum update
 apt-get install curl
 yum install curl
-curl -O https://pastebin.obir.ninja/tardis
+curl -O https://tardis-script.com/tardis
 chmod +x tardis
 ./tardis
